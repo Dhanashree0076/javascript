9c40865f17d4de010167a62222b368f1d4c24993
Dhanashree0076 <dhanshrigadhave3@gmail.com>
2025-04-16 07:19:29 -0700

    data types in js

---
+++ Day3/script.js
@@ -0,0 +1,21 @@
+// Number 
+
+ let PI = 3.14
+ let pinCode = 412802
+ let mobileNumber = 6743679838
+ let age = 30
+ let radius = 7
+ let OTP =2346
+
+
+ // String
+
+  let email_id =abc@gmail.com
+  let college_name ="ATC BHOR"
+  let address ="AT POST MHAVSHI"
+
+  //
+
+
+
+
