46caa13963a1935b30a668b02ea6a5a2f00588e6
Dhanashree0076 <dhanshrigadhave3@gmail.com>
2025-04-17 06:49:25 -0700

    learn with js

--- Day6/script.js
+++ Day6/script.js
@@ -1,6 +1,13 @@
-let age =10:
+let age = 10
 
-let result=age >=18? "able to vote" :"not able to vote";
+let result=age >=18? "able to vote":"not able to vote"
 
 console.log(result)
+
+let number="m"
+
+let result = gender=="m"?"male":"female"
+
+console.log(result)
+
 
